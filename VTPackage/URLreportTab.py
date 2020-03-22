594fa5fe2c4043558a00cb873c3d45ea6d7b9777
tomgo <tomgold182@gmail.com>
2020-03-22 17:01:47 +0200

    Added an IP investigation tab various bug fixes

--- VTPackage/URLreportTab.py
+++ VTPackage/URLreportTab.py
@@ -5,16 +5,12 @@ class URLreportTab:
     def __init__(self,root,frame, vtClient):
         self.root = root
         self.frame = frame
-        self.mainVTURLframe = ttk.LabelFrame(frame, text=' Virus Total UI!')
+        self.mainVTURLframe = ttk.LabelFrame(frame, text=' URL report tab!')
 
 
         # using the tkinter grid layout manager
         self.mainVTURLframe.grid(column=0, row=0, padx=8, pady=4)
         ttk.Label(self.mainVTURLframe, text="URL:").grid(column=0, row=0, sticky='W')  #What does sticky does?      Sticky sayes where to stick the label to : N,S,E,W
-        urlEntry = ttk.Entry(self.mainVTURLframe)
-        urlEntry.grid(column=1, row=0, sticky='E')
-
-        ttk.Label(self.mainVTURLframe, text="URL:").grid(column=0, row=0, sticky='W')
         urlEntry = ttk.Entry(self.mainVTURLframe)
         urlEntry.grid(column=1, row=0, sticky='E')
 
@@ -37,13 +33,14 @@ class URLreportTab:
         ErrorEntry = ttk.Entry(notificationFrame, width=20, textvariable=Error, state='readonly')
         ErrorEntry.grid(column=1, row=0, sticky='W')
 
-        def cleanErrorMessage():  # We could have been doing this without a function, but it is more neat that way
+        def _cleanErrorMessage():  # We could have been doing this without a function, but it is more neat that way
             Error.set("")
 
 
-        def getReport():
+        def _getReport():
+            # the _ notation before a function means that this function is internal to the class only. As python cannot really prevent you from using it outside the class (as C# for example) the notation is being used to warn other developers not to call this function outside the class
             try:
-                cleanErrorMessage() # Starting with cleaning the error message bar
+                _cleanErrorMessage() # Starting with cleaning the error message bar
                 if not urlEntry.get():
                     print('Please enter a URL')
                     Error.set("Please enter a URL!")
@@ -66,6 +63,6 @@ class URLreportTab:
                 print(e)
                 Error.set(e)
 
-        checkURLinVTButton = ttk.Button(self.mainVTURLframe, text='Check in VT!', command=getReport).grid(column=2, row=0)
+        checkURLinVTButton = ttk.Button(self.mainVTURLframe, text='Check in VT!', command=_getReport).grid(column=2, row=0)
 
 

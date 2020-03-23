f94f81f6512f7d8d05e36af2098a722b7674073d
tomgo <tomgold182@gmail.com>
2020-03-23 09:37:18 +0200

    added padding to the UI increased Error pane size

--- VTPackage/URLreportTab.py
+++ VTPackage/URLreportTab.py
@@ -1,6 +1,7 @@
 from tkinter import ttk
 from tkinter import StringVar
 
+
 class URLreportTab:
     def __init__(self,root,frame, vtClient):
         self.root = root
@@ -24,13 +25,13 @@ class URLreportTab:
         detectionsEntry = ttk.Entry(self.mainVTURLframe, width=20, textvariable=detections, state='readonly')
         detectionsEntry.grid(column=1, row=2, sticky='W')
 
-        notificationFrame = ttk.LabelFrame(self.frame, text=' Notifications', width=40)
+        self.notificationFrame = ttk.LabelFrame(self.frame, text=' Notifications', width=40)
         # using the tkinter grid layout manager
-        notificationFrame.grid(column=0, row=1, padx=8, pady=10, sticky='W')
+        self.notificationFrame.grid(column=0, row=1, padx=8, pady=10, sticky='W')
 
-        ttk.Label(notificationFrame, text="Errors:").grid(column=0, row=0, sticky='W')  # <== increment row for each
+        ttk.Label(self.notificationFrame, text="Errors:").grid(column=0, row=0, sticky='W')  # <== increment row for each
         Error = StringVar()
-        ErrorEntry = ttk.Entry(notificationFrame, width=20, textvariable=Error, state='readonly')
+        ErrorEntry = ttk.Entry(self.notificationFrame, width=60, textvariable=Error, state='readonly')
         ErrorEntry.grid(column=1, row=0, sticky='W')
 
         def _cleanErrorMessage():  # We could have been doing this without a function, but it is more neat that way
@@ -64,5 +65,13 @@ class URLreportTab:
                 Error.set(e)
 
         checkURLinVTButton = ttk.Button(self.mainVTURLframe, text='Check in VT!', command=_getReport).grid(column=2, row=0)
+
+        #Instead of setting padding for each UI element, we can just iterate through the children of the main UI object.
+        for child in self.mainVTURLframe.winfo_children():
+            child.grid_configure(padx=4, pady=2)
+        for child in self.notificationFrame.winfo_children():
+            child.grid_configure(padx=4, pady=2)
+
+
 
 
